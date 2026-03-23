4d012e36ba4eb3967b75cfd1d339b31353d65ee1
yessenamandiana-ux <yessenamandiana@gmail.com>
2026-03-23 21:17:27 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
+import joblib
 
-st.title("Работает")
+st.title("Проверка модели")
+
+try:
+    model = joblib.load("application_train.joblib")
+    st.success("Модель успешно загружена")
+    st.write(type(model))
+except Exception as e:
+    st.error(f"Ошибка загрузки модели: {e}")
