35a3063728e5fe7c21e2905e80f4b72b02d9b6fc
yessenamandiana-ux <yessenamandiana@gmail.com>
2026-03-23 21:23:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ import joblib
 
 st.title("Проверка модели")
 
+@st.cache_resource
+def load_model():
+    return joblib.load("application_train.joblib")
+
 try:
-    model = joblib.load("application_train.joblib")
-    st.success("Модель успешно загружена")
+    model = load_model()
+    st.success("Модель загружена")
     st.write(type(model))
 except Exception as e:
-    st.error(f"Ошибка загрузки модели: {e}")
+    st.error(f"Ошибка: {e}")
